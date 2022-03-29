c126759b92682ad65a95355d66940312ddc2ed72
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-03-29 13:14:01 +0530

    Absolute Diff

---
+++ src/Quest/Absolute_Diff.java
@@ -0,0 +1,28 @@
+package Quest;
+
+import java.util.Scanner;
+
+public class Absolute_Diff {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int num=sc.nextInt();
+        int diff=sc.nextInt();
+
+        System.out.println(findCount(n, arr,num,diff));
+    }
+    public static int findCount(int n,int[] arr,  int num, int diff) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (Math.abs(num - arr[i]) <= diff)
+                count++;
+
+        }
+        return count >= 0 ? count : -1;
+    }
+}
